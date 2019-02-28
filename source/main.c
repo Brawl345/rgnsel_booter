528dc7e3ae17c5818965909feb7c3f3a5f133bb5
Andreas Bielawski <brawl98@outlook.com>
2019-02-28 23:44:57 +0100

    Shorten code

--- source/main.c
+++ source/main.c
@@ -1,12 +1,11 @@
 #include <gccore.h>
 #include <stdlib.h>
-#define TITLE_ID(x,y) (((u64)(x) << 32) | (y))
 
 int main(void) {
     VIDEO_Init();
-    WII_LaunchTitle(TITLE_ID(0x00010008,0x48414C45)); // USA
-    WII_LaunchTitle(TITLE_ID(0x00010008,0x48414C50)); // Europe
-    WII_LaunchTitle(TITLE_ID(0x00010008,0x48414C4A)); // Japan
-    WII_LaunchTitle(TITLE_ID(0x00010008,0x48414C4B)); // Korea
+    WII_LaunchTitle(0x0001000848414C45); // USA
+    WII_LaunchTitle(0x0001000848414C50); // Europe
+    WII_LaunchTitle(0x0001000848414C4A); // Japan
+    WII_LaunchTitle(0x0001000848414C4B); // Korea
     exit(0);
 }
